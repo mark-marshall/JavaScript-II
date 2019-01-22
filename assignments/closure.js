1aa7720d72eb6df60736998103e169650a9e9432
mark-marshall <mark.marshallgp@gmail.com>
2019-01-22 17:17:44 +0000

    [MVP] Reformated code

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,34 +32,40 @@ son();
 
 const counter = () => {
   let count = 0;
-  
-    return function () {
-        return count++;
-      }
+
+  return function() {
+    return count++;
   };
-  
-  const newCounter = counter();
-  newCounter();
-  newCounter();
-  newCounter();
-  
+};
+
+const newCounter = counter();
+newCounter();
+newCounter();
+newCounter();
+
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 // const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
 //};
 
 const counterFactory = () => {
-  let countr =0;
+  let countr = 0;
 
   return {
-    increment: function (){return countr++;},
-    decrement: function (){return countr--;},
-    checker: function (){return countr;},
-  }
+    increment: function() {
+      return countr++;
+    },
+    decrement: function() {
+      return countr--;
+    },
+    checker: function() {
+      return countr;
+    }
+  };
 };
 
 let counterTime = counterFactory();
